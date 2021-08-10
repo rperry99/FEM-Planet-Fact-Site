ea8980a605c3b61c542820c15740a87fa5bd80c3
Russ <rperry1886@gmail.com>
2021-08-10 15:23:53 -0400

    Make nav fully responsive

--- dist/app.js
+++ dist/app.js
@@ -6,3 +6,12 @@ hamburger.addEventListener('click', function () {
   planetLinks.classList.toggle('show-content');
   hamburger.classList.toggle('dim');
 });
+
+// Hover effects for the desktop nav.
+if (window.innerWidth >= 1023) {
+  $('.planet-link .name p').hover(function (e) {
+    $(this)
+      .closest('.planet-link')
+      .toggleClass(`${this.innerHTML.toLowerCase()}`, e.type == 'mouseenter');
+  });
+}
