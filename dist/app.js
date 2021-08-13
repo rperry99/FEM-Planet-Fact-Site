7dc57d57af4064e7fea67494ab9fa21eddea7e66
Russ <rperry1886@gmail.com>
2021-08-13 12:52:30 -0400

    Get the overview, structure, and surface buttons working

--- dist/app.js
+++ dist/app.js
@@ -19,6 +19,7 @@ if (window.innerWidth >= 1023) {
 }
 
 // Changing Planets
+let currentPlanet = 'Mercury';
 const planetsJSON = (function () {
   var json = null;
   $.ajax({
@@ -42,14 +43,18 @@ function changePlanet(planet) {
         $('#hamburger').toggleClass('dim');
       }
 
-      // Force the overview to be selected when changing planets.
+      // Force the overview to be selected when changing planets and hide the overlay
       $('#overview').prop('checked', true);
+      removeSurfaceOverlay();
+
+      // Set the currentPlanet variable to be the current planet.
+      currentPlanet = planet;
 
       // Change the title of the page
       document.title = `Frontend Mentor | ${planet}`;
 
       // Change the image when switching planets
-      $('#planet-image img')
+      $('#planet-image #planet')
         .attr('src', `${planetsJSON[key].images.planet}`)
         .removeClass()
         .addClass(`${planet.toLowerCase()}`);
@@ -72,8 +77,66 @@ function changePlanet(planet) {
 }
 
 function changePlanetContent(tab) {
-  // Change planet image
-  // If the selection is "Structure" show the image overlay
+  //   $('#planet-image img').attr(
+  //     'src',
+  //     `${planetsJSON[currentPlanet].images[tab]}`
+  //   );
+  for (var key in planetsJSON) {
+    if (planetsJSON[key].name == currentPlanet) {
+      if (tab === 'internal') {
+        // Change the planet image
+        $('#planet-image #planet').attr(
+          'src',
+          `${planetsJSON[key].images.internal}`
+        );
+
+        // Hide the surface overlay
+        removeSurfaceOverlay();
+
+        // Change the text.
+        $('#planet-text').text(`${planetsJSON[key].structure.content}`);
+        $('#wiki-article a').attr(
+          'href',
+          `${planetsJSON[key].structure.source}`
+        );
+      } else if (tab === 'planet') {
+        // Change the planet image
+        $('#planet-image #planet').attr(
+          'src',
+          `${planetsJSON[key].images.planet}`
+        );
+
+        // Hide the surface overlay
+        removeSurfaceOverlay();
+
+        // Change the text.
+        $('#planet-text').text(`${planetsJSON[key].overview.content}`);
+        $('#wiki-article a').attr(
+          'href',
+          `${planetsJSON[key].overview.source}`
+        );
+      } else if (tab === 'geology') {
+        // Reset the planet image to be the surface
+        $('#planet-image #planet').attr(
+          'src',
+          `${planetsJSON[key].images.planet}`
+        );
+
+        // Display the container that holds the surface png
+        $('#planet-image #surface-overlay')
+          .addClass('show-content')
+          .attr('src', `${planetsJSON[key].images.geology}`);
+
+        // Change the text.
+        $('#planet-text').text(`${planetsJSON[key].geology.content}`);
+        $('#wiki-article a').attr('href', `${planetsJSON[key].geology.source}`);
+      }
+    }
+  }
   // Change the planet text
   // Change the wiki source
+}
+
+function removeSurfaceOverlay() {
+  $('#planet-image #surface-overlay').removeClass('show-content');
 }
